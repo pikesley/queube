312166b47033152d885cfa5720e72d46fefc81be
pikesley <sam.pikesley@demandlogic.co.uk>
2021-03-07 21:52:24 +0000

    Get better buttons

--- static/js/controls.js
+++ static/js/controls.js
@@ -1,9 +1,8 @@
-var neutral = "M18 18h-12c-3.311 0-6-2.689-6-6s2.689-6 6-6h12.039c3.293.021 5.961 2.701 5.961 6 0 3.311-2.688 6-6 6zm-6-10c2.208 0 4 1.792 4 4s-1.792 4-4 4-4-1.792-4-4 1.792-4 4-4z"
-var paths = {
-    "active": "M6 18h12c3.311 0 6-2.689 6-6s-2.689-6-6-6h-12.039c-3.293.021-5.961 2.701-5.961 6 0 3.311 2.688 6 6 6zm12-10c-2.208 0-4 1.792-4 4s1.792 4 4 4 4-1.792 4-4-1.792-4-4-4z",
-    "inactive": "M18 18h-12c-3.311 0-6-2.689-6-6s2.689-6 6-6h12.039c3.293.021 5.961 2.701 5.961 6 0 3.311-2.688 6-6 6zm-12-10c2.208 0 4 1.792 4 4s-1.792 4-4 4-4-1.792-4-4 1.792-4 4-4z",
-    "activating": neutral,
-    "deactivating": neutral
+lookups = {
+    "active": "pause",
+    "inactive": "play",
+    "activating": "play",
+    "deactivating": "pause"
 }
 
 var socket
@@ -13,16 +12,15 @@ $(document).ready(function () {
 
     socket.on('phase', function (msg) {
         currentPhase = msg.data
+        buttonContent = lookups[currentPhase]
 
-        $('#switch path').attr('d', paths[currentPhase])
-        $('#switch').removeClass().addClass(currentPhase)
+        $('#control-button #button').html(buttons[buttonContent])
+        $('#control-button').removeClass().addClass(currentPhase)
     })
 
-    // socket.on('colour', function (msg) {
-    //     console.log(msg.data)
-    //     $('body').css('background', `rgb(${msg.data})`)
-    //     $('h1').text('background', msg.data)
-    // })
+    socket.on('colour', function (msg) {
+        $('h1').css('color', `rgb(${ msg.data })`)
+    })
 });
 
 function switchLights() {
